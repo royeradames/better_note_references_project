da320d14dc06a28267c16fb6bb22a43ae288064e
royeradames <royeraadames@gmail.com>
2020-11-03 12:09:00 -0500

    set up knex cleaner to clean up database when seeds run

--- db/seeds/000_knex_cleaner.js
+++ db/seeds/000_knex_cleaner.js
@@ -1,14 +1,17 @@
-import cleaner from "knex-cleaner"
+//import cleaner from "knex-cleaner"
+const cleaner = require("knex-cleaner")
 
 function cleanTables(knex){
-    return cleaner.clean(knex, {
-        mode: "truncate",
-        restartIdentify: true, //ask Postgresql to reset the primary keys back to 0
-        ignoreTables: ["knex_migrations", "knex_migrations_lock"],
-    })
+    return cleaner
+        .clean(knex, {
+            mode: "truncate",
+            restartIdentify: true, //ask Postgresql to reset the primary keys back to 0
+            ignoreTables: ["knex_migrations", "knex_migrations_lock"],
+        })
 }
 
-export default function(knex){
+// export default function(knex){
+exports.seed = function (knex){
     /*
         a recent version of sqlite3 broke knex-cleaner functionality when foreign keys are enabled, so we're temporarily disabling foreign keys when running the seeds against sqlite3.
     */

3ae9ba995a041695c88f62f6f11789d04d32cf1d
royeradames <royeraadames@gmail.com>
2020-11-08 00:05:05 -0500

    check for 200 and library object exist in response when valid id given

--- api/libraries/libraries.test.js
+++ api/libraries/libraries.test.js
@@ -1,15 +1,22 @@
+//import 
 const server = require("../server")
 const request = require("supertest")
-
 // prep test database
 const prepTestDB = require("../../helpers/prepTestDB")
 
 // apply a static state for all tests
 beforeEach(prepTestDB)
 
-it("200 return all libraries", async function(){
+it("/ 200 return all libraries", async function(){
     const res = await request(server).get("/libraries/")
 
     expect(res.body.libraries).toEqual(expect.any(Object))
+    expect(res.status).toBe(200)
+})
+
+it("/:id 200 valid id found in server", async () => {
+    const res = await request(server).get("/libraries/1")
+
+    expect(res.body.library).toEqual(expect.any(Object))
     expect(res.status).toBe(200)
 })

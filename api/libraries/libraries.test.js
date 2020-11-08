ea929b2af9f8b1ad797c56e858798421d93c4c04
royeradames <royeraadames@gmail.com>
2020-11-07 22:51:41 -0500

    test for return status 200 and a object name libraries

---
+++ api/libraries/libraries.test.js
@@ -0,0 +1,15 @@
+const server = require("../server")
+const request = require("supertest")
+
+// prep test database
+const prepTestDB = require("../../helpers/prepTestDB")
+
+// apply a static state for all tests
+beforeEach(prepTestDB)
+
+it("200 return all libraries", async function(){
+    const res = await request(server).get("/libraries/")
+
+    expect(res.body.libraries).toEqual(expect.any(Array))
+    expect(res.status).toBe(200)
+})

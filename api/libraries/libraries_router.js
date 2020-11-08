e74fb7c0f96ef8af89c581742f516c3be4248724
royeradames <royeraadames@gmail.com>
2020-11-08 00:08:12 -0500

    📦when given valid id, and return  record by id

--- api/libraries/libraries_router.js
+++ api/libraries/libraries_router.js
@@ -1,6 +1,11 @@
+//init express router
 const router = require("express").Router()
 
+//import libraries_model
 const Libraries = require("./libraries_model")
+
+//import dependencies
+const { check, validationResult } = require('express-validator')
 
 router.get("/", async (req, res, next) => {
     try {
@@ -11,11 +16,30 @@ router.get("/", async (req, res, next) => {
         next(error)
     }
 })
-// router.post("/:id", async () => {
-
-// })
+router.post("/:id", check_id, async (req, res, next) => {
+    res.status(200).json({library: req.library})
+})
 // router.post("/:name", async () => {
 
 // })
+
+//local middleware
+async function check_id(req, res, next){
+    try {
+        const id = req.params.id
+        //use cases
+        // not a number
+        console.log((typeof Number(id)))
+        const id_not_number = (typeof Number(id)) != "number" 
+        if(id_not_number) res.status(404).json({error: "id must be a number"})
+
+        const library = await Libraries.find_by_id()
+
+        req.library = library
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
 
 module.exports = router

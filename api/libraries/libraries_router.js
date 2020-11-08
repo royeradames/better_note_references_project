82544543d4e01ce1baa602f1cfd240a4fca86eea
royeradames <royeraadames@gmail.com>
2020-11-08 16:13:39 -0500

    📦receive name of library and return a matching record with that name

--- api/libraries/libraries_router.js
+++ api/libraries/libraries_router.js
@@ -17,17 +17,16 @@ router.get("/", async (req, res, next) => {
         next(error)
     }
 })
-router.post("/:id", check_id, async (req, res, next) => {
+router.get("/:id", check_id, async (req, res, next) => {
     res.status(200).json({library: req.library})
 })
-// router.post("/:name", async () => {
-
-// })
+router.get("/findlibrarybyname/:name", check_name, async (req, res) => {
+    res.status(200).json({library: req.library_name})
+})
 
 //local middleware
 async function check_id(req, res, next){
     try {
-
         const id = req.params.id
         const library = await Libraries.find_by_id(id)
         
@@ -43,4 +42,21 @@ async function check_id(req, res, next){
     }
 }
 
+async function check_name(req, res, next){
+    try {
+        const name = req.params.name
+        const library_name = await Libraries.find_by_name(name)
+
+        const library_name_found = library_name.length
+        
+        if (library_name_found) {
+            req.library_name = library_name
+            next()
+        } else {
+            res.status(404).json({error: "name not found"})            
+        }
+    } catch (error) {
+        next(error)
+    }
+}
 module.exports = router

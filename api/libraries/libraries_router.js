71de85cf264fce4de9ab903e9909aae3ca221a1d
royeradames <royeraadames@gmail.com>
2020-11-08 12:56:42 -0500

    👌 remove unuse code

--- api/libraries/libraries_router.js
+++ api/libraries/libraries_router.js
@@ -9,6 +9,8 @@ const { check, validationResult } = require('express-validator')
 
 router.get("/", async (req, res, next) => {
     try {
+        console.log(`inside get /`)
+
         const libraries = await Libraries.get_all_libraries()
         
         res.status(200).json({libraries})
@@ -17,6 +19,7 @@ router.get("/", async (req, res, next) => {
     }
 })
 router.post("/:id", check_id, async (req, res, next) => {
+    console.log(`inside post /:id`)
     res.status(200).json({library: req.library})
 })
 // router.post("/:name", async () => {
@@ -26,14 +29,11 @@ router.post("/:id", check_id, async (req, res, next) => {
 //local middleware
 async function check_id(req, res, next){
     try {
-        const id = req.params.id
-        //use cases
-        // not a number
-        console.log((typeof Number(id)))
-        const id_not_number = (typeof Number(id)) != "number" 
-        if(id_not_number) res.status(404).json({error: "id must be a number"})
 
-        const library = await Libraries.find_by_id()
+        const id = req.params.id
+        const library = await Libraries.find_by_id(id)
+        //if id matches none in database responde with invalid id
+        if (library.length === 0) res.status(404).json({error: "Invalid ID"})
 
         req.library = library
         next()

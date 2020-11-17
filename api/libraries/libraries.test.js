52b2ef6f0309e579187a131fca283a8b9dfa4f68
royeradames <royeraadames@gmail.com>
2020-11-17 10:22:19 -0500

    🐛fix: test fails

--- api/libraries/libraries.test.js
+++ api/libraries/libraries.test.js
@@ -7,7 +7,7 @@ const prepTestDB = require("../../helpers/prepTestDB")
 // apply a static state for all tests
 beforeEach(prepTestDB)
 
-it("/ 200 return all libraries", async function(){
+it("/ 200 return all libraries", async () =>{
     const res = await request(server).get("/libraries/")
 
     expect(res.status).toBe(200)
@@ -15,7 +15,7 @@ it("/ 200 return all libraries", async function(){
 })
 
 it("/:id 200 valid id found in server", async () => {
-    const res = await request(server).post("/libraries/1")
+    const res = await request(server).get("/libraries/1")
 
     expect(res.status).toBe(200)
     expect(res.body.library).toEqual(expect.any(Object))
@@ -29,14 +29,14 @@ it("/:id 404 invalid id", async () => {
 })
 
 it("/findlibrarybyname/:name 200 given name IS ON database", async () => {
-    const res = await request(server).get("/libraries/findlibrarybyname/express")
+    const res = await request(server).get("/libraries/findlibrarybyname/express.js")
 
     expect(res.status).toBe(200)
-    expect(res.body.library).toMatch(expect.any(Object))
+    expect(res.body.library).toEqual(expect.any(Array))
 })
 it("/findlibrarybyname/:name 404  given name NOT on database", async () => {
     const res = await request(server).get("/libraries/findlibrarybyname/notalibraryonthedatabase")
 
     expect(res.status).toBe(404)
-    expect(res.body.error).toMatch(expect.any(String))
+    expect(res.body.error).toEqual(expect.any(String))
 })

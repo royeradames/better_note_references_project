84577143ffb46a0487b083aa03cd8ed7b84e0d1c
royeradames <royeraadames@gmail.com>
2020-11-22 15:36:31 -0500

    🔬check: already name libraries are not duplicated

--- api/libraries/tests/post_new_library.test.js
+++ api/libraries/tests/post_new_library.test.js
@@ -24,3 +24,19 @@ describe("200", () => {
     })
 
 })
+
+describe('client errors', () => {
+    it("add new library to the db", async () => {
+        // call server
+        const res = await request(server).post(url).send({
+            name: "express",
+            description: "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.",
+            tag_name: "backend",
+            link: "https://expressjs.com/",
+        })
+
+        //check reserver responds
+        expect(res.status).toBe(406)
+        expect(res.body).toMatch(/already exists/i)
+    })
+})

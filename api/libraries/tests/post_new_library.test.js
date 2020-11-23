2bd313f10514d91675f23713fc74d72922ab9a2b
royeradames <royeraadames@gmail.com>
2020-11-22 19:17:43 -0500

    🔬check: no acceptance of invalid name

--- api/libraries/tests/post_new_library.test.js
+++ api/libraries/tests/post_new_library.test.js
@@ -9,24 +9,25 @@ beforeEach(prepTestDB)
 
 const url = "/libraries/"
 
-describe("200", () => {
+describe("success", () => {
     it("add new library to the db", async () => {
         // call server
         const res = await request(server).post(url).send({
-            name: "express",
-            description: "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.",
+            name: "testing correctly",
+            description: "This is a test, and everything should work just fine.",
             tag_name: "backend",
-            link: "https://expressjs.com/",
+            link: "https://test.com/",
         })
 
         //check reserver responds
+        console.log(res.body)
         expect(res.status).toBe(200)
     })
 
 })
 
 describe('client errors', () => {
-    it("add new library to the db", async () => {
+    it("add new library", async () => {
         // call server
         const res = await request(server).post(url).send({
             name: "express",
@@ -35,8 +36,21 @@ describe('client errors', () => {
             link: "https://expressjs.com/",
         })
 
-        //check reserver responds
+        //check server responds
         expect(res.status).toBe(406)
         expect(res.body).toMatch(/already exists/i)
+    })
+    it('invalid name', async () => {
+        const res = await request(server).post(url).send({
+            name: "express.js",
+            description: "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.",
+            tag_name: "backend",
+            link: "https://expressjs.com/",
+        })
+
+        //check server responds
+        expect(res.status).toBe(404)
+        expect(res.body[0].value).toMatch(/express.js/i)
+        expect(res.body[0].param).toMatch(/name/i)
     })
 })

f74e395b7ad08ebf43ad6aee3ba4dd15a52d55e8
royeradames <royeraadames@gmail.com>
2020-11-24 19:29:23 -0500

    📦if there is no tags resp 404 no tags

--- api/tags/tags_router.js
+++ api/tags/tags_router.js
@@ -7,14 +7,21 @@ const Tags = require("./tags_model")
 //import dependencies
 const {body, validationResult} = require ("express-validator")
 
-// return all tags
+// return all tags  
 router.get("/", async (req, res, next) => {
     try {
        //get all tags from db
        const tags = await Tags.get_all_tags() 
 
-       //return tags
-       res.status(200).json(tags)
+       const is_tags_here = tags.length
+       if(is_tags_here){
+           //return tags
+           res.status(200).json(tags)
+       }
+       else{
+           //if there is no tags, say so
+           res.status(404).json("No tags exists.")
+       }
     } catch (error) {
        next(error) 
     }

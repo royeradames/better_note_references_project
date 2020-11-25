c89feab834017a6c32b1ee74f1392ba79fb806f3
royeradames <royeraadames@gmail.com>
2020-11-25 18:30:05 -0500

    📦validate params ID to be an integer, if not res 404

--- api/useful_links/useful_links_route.js
+++ api/useful_links/useful_links_route.js
@@ -4,6 +4,9 @@ const router = require("express").Router()
 // import Useful_links_model
 const Useful_links = require("./useful_links_model")
 
+// import back-end validation tools
+const {body, param, validationResult} = require ("express-validator")
+
 // return all useful_links
 router.get("/", async (req, res, next) => {
     try {
@@ -24,14 +27,34 @@ router.get("/", async (req, res, next) => {
 })
 
 // get link by id
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", [
+    param("id")
+        .isInt().withMessage("ID must be an integer")
+        ,
+], handle_fail_valitions, async (req, res, next) => {
     try {
         //get link from db
        const useful_link = (await Useful_links.by_id(req.params.id))[0]
-       res.status(200).json(useful_link)
+       if(useful_link){
+           res.status(200).json(useful_link)
+       }
+       else{
+           res.status(404).json("Link not found")
+       }
     } catch (error) {
         next(error)
         
     }
 })
+
+//local middleware
+function handle_fail_valitions(req, res, next){
+    // handle fail validations
+    const errors = validationResult(req)
+    const is_errors = !errors.isEmpty()
+    if (is_errors) return res.status(404).json(errors.array()[0])
+
+    //no fail validation, then go to the next middleware
+    next()
+}
 module.exports = router

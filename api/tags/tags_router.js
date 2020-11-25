86b81915d6e028c6dddc762694a32bc7176f89cb
royeradames <royeraadames@gmail.com>
2020-11-24 21:07:07 -0500

    📦post /newtag creates new tags

--- api/tags/tags_router.js
+++ api/tags/tags_router.js
@@ -4,7 +4,7 @@ const router = require("express").Router()
 //import tags_model
 const Tags = require("./tags_model")
 
-//import dependencies
+//import validation checker
 const {body, validationResult} = require ("express-validator")
 
 // return all tags  
@@ -27,4 +27,32 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+// create new tag
+router.post("/newtag", [
+    body("name")
+        .isAlpha().withMessage("Only letters for tag name")
+        ,
+], handle_fail_valitions, async (req, res, next) => {
+    try {
+        // create new tag on db
+        const new_tag = (await Tags.post_new_tag(req.body.name))[0] 
+
+        //return new create tag to the client
+        res.status(200).json({new_tag: new_tag.name})
+    } catch (error) {
+        next(error)
+        
+    }
+})
+
+//local middleware
+function handle_fail_valitions(req, res, next){
+    // handle fail validations
+    const errors = validationResult(req)
+    const is_errors = !errors.isEmpty()
+    if (is_errors) return res.status(404).json(errors.array())
+
+    //no fail validation, then go to the next middleware
+    next()
+}
 module.exports = router

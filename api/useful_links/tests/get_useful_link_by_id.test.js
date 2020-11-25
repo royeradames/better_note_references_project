d1e7f828934905375f0784352abf5afa3f013d03
royeradames <royeraadames@gmail.com>
2020-11-25 17:50:46 -0500

    🔬check: server resp valid with found link by id

--- api/useful_links/tests/get_useful_link_by_id.test.js
+++ api/useful_links/tests/get_useful_link_by_id.test.js
@@ -8,9 +8,21 @@ const db = require("../../../db/dbconfig")
 // get function that can resets the database
 const prepTestDB = require("../../../helpers/prepTestDB")
 
-// apply db reset for each test
-const url = "/useful_links/:id"
+// apply a static db for each tests
+beforeEach(prepTestDB)
 
-it.todo("get library by id")
+// apply db reset for each test
+const url = "/useful_links/"
+
+it("get library by id", async () => {
+    // get server resp
+    const res = await request(server).get(`${url}/1`)
+
+    //validate server resp
+    expect(res.status).toBe(200)
+    expect(res.body.id).toBe(1)
+    expect(res.body.name).toMatch(/node.js docum/i)
+    expect(res.body.description).toMatch(/the official api reference documentation/i)
+})
 it.todo("if id not found then resp with id not found")
 it.todo("id must be an id")

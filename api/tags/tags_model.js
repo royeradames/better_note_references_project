8f47a5097bdea308125e8aadf7576f79939c8be8
royeradames <royeraadames@gmail.com>
2020-11-24 21:05:55 -0500

    📦create new tag on db and return the new tag

--- api/tags/tags_model.js
+++ api/tags/tags_model.js
@@ -1,9 +1,14 @@
 const db = require("../../db/dbConfig")
 
 module.exports = {
-    get_all_tags
+    get_all_tags,
+    post_new_tag,
 }
 
 async function get_all_tags(){
     return db("tags").select()
+}
+async function post_new_tag(name){
+    await db("tags").insert({name})
+    return db("tags").where({name})
 }

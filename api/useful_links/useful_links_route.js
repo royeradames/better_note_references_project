d7568b9ffe317dd1d926df6c4d20219d2ef1e976
royeradames <royeraadames@gmail.com>
2020-11-25 16:39:10 -0500

    📦resp with 404 no links when there is no links

--- api/useful_links/useful_links_route.js
+++ api/useful_links/useful_links_route.js
@@ -10,11 +10,16 @@ router.get("/", async (req, res, next) => {
         // get all links on db
        const all_useful_links = await Useful_links.get_all_useful_links()
 
-        //return all links on 
-        res.status(200).json(all_useful_links)
+       const is_have_links = all_useful_links.length
+       if(is_have_links){
+           //return all links on 
+           res.status(200).json(all_useful_links)
+       }
+       else{
+           res.status(404).json("no links")
+       }
     } catch (error) {
         next(error)
-        
     }
 })
 

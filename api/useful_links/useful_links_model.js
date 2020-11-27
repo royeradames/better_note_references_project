388b7a4d3613622e3de9fb1552dab08c6c3bb13d
royeradames <royeraadames@gmail.com>
2020-11-26 19:36:36 -0500

    📦insert link data to db

--- api/useful_links/useful_links_model.js
+++ api/useful_links/useful_links_model.js
@@ -6,6 +6,7 @@ module.exports = {
     get_all,
     by_id,
     by_name,
+    new_resource,
 }
 
 function by_id(id){
@@ -16,4 +17,8 @@ function get_all(){
 }
 function by_name(name){
     return db("useful_links").where({name})
+}
+async function new_resource(link_data){
+   await db("useful_links").insert(link_data) 
+   return (await db("useful_links").where({name: link_data.name}))[0]
 }

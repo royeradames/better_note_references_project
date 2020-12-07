acbb3e871cd4a5ae04c31335b7245c55dc3682c2
royeradames <royeraadames@gmail.com>
2020-12-07 13:10:07 -0500

    📦setup postgrest production db files

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+const pgConnection = process.env.DATABASE_URL 
 module.exports = {
 
   development: {
@@ -40,36 +40,18 @@ module.exports = {
       }
     },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: pgConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
+    },
+    seeds: {
+      dicrectory: "./db/seeds"
     }
   }
 

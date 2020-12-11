b8a56f477f70483b52219b4a04013893c0e6ef51
royeradames <royeraadames@gmail.com>
2020-12-11 07:54:15 -0500

    🔬check all quert strings work

--- api/libraries/tests/get_all_libraries.test.js
+++ api/libraries/tests/get_all_libraries.test.js
@@ -30,8 +30,22 @@ it(" 404 no libraries", async() => {
 
     //call server
     const res = await request(server).get(url)
-
     //validate server response
     expect(res.status).toBe(404)
     expect(res.body.error).toMatch(/No libraries at this moment/i)
 })
+
+it("check all query strings work", async () => {
+    //request resource
+    const res = await request(server).get(`${url}?limit=5&order=desc&offset=1&avoid=27`)
+
+    //validate resp 
+    expect(res.status).toBe(200)
+    expect(res.body.length).toBe(5)
+    expect(res.body[0].id).toBe(28)
+    expect(res.body[1].id).not.toBe(27)
+
+})
+it.todo("check that all query returns all labraries")
+it.todo("avoid a list of libraries")
+it.todo("check all validations work")

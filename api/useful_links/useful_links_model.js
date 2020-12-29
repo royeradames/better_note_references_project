92fa9e15daf1d82363c238aec52eb619a8975d1c
royeradames <royeraadames@gmail.com>
2020-12-29 13:11:45 -0500

    👌change names to be less critic, return 1 result for id and all

--- api/useful_links/useful_links_model.js
+++ api/useful_links/useful_links_model.js
@@ -4,21 +4,21 @@ const db = require("../../db/dbConfig")
 //export functions
 module.exports = {
     get_all,
-    by_id,
-    by_name,
+    get_by_id,
+    get_by_name,
     new_resource,
 }
 
-function by_id(id){
-    return db("useful_links").where({id})
+function get_by_id(id){
+    return db("useful_links").where({id}).first()
 }
 function get_all(){
     return db("useful_links")
 }
-function by_name(name){
-    return db("useful_links").where({name})
+function get_by_name(name){
+    return db("useful_links").where({name}).first()
 }
 async function new_resource(link_data){
    await db("useful_links").insert(link_data) 
-   return (await db("useful_links").where({name: link_data.name}))[0]
+   return await get_by_name(link_data.name)
 }

bbf9ab8b0ab0aa0fb988d9bcc2b146e5f34bf392
royeradames <royeraadames@gmail.com>
2020-12-30 11:16:32 -0500

    📦setup updat_tag test

---
+++ api/tags/tests/update_tag.test.js
@@ -0,0 +1,16 @@
+//setup server instance and request option
+const server = require("../../server")
+const request = require("supertest")
+
+//database
+const db = require("../../../db/dbConfig")
+
+//prep test database
+const prepTestDB = require("../../../helpers/prepTestDB")
+
+//apply a static db for each tests
+beforeEach(prepTestDB)
+
+//local global endpoint url
+const url = "/tags/"
+
